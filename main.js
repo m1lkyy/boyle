bdf007e2582c693ee417c905179a3328100c4a1b
m1lkyy <82616233+m1lkyy@users.noreply.github.com>
2022-06-09 11:02:24 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.main-menu');
+const navLogo = document.querySelector('.name-logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 576 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
